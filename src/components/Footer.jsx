7708c7859bffde2d5bc6f14c46c3058ab14d71b1
Eyesore123 <joni.putkinen@protonmail.com>
2025-03-03 14:34:30 +0200

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../src/Index.css'
 import * as FooterStyles from './Footer.module.css'
-import Icon2 from '../../src/assets/pnp-power_musta.png'
+import Icon2 from '../../public/icon.png'
 import Envelope from '../../src/assets/Envelope.svg'
 import Phone from '../../src/assets/Phone.svg'
 
@@ -9,20 +9,34 @@ export default function Footer() {
   return (
     <div className={FooterStyles.footercontainer}>
         <img src={Icon2} alt="PNP-Power icon2" className={FooterStyles.icon2} />
-        <div className='footer'>
+        <div className={FooterStyles.footer}>
             <span className='m-2'>
-              <li className='list-none flex justify-start items-center gap-6 font-bold'>
+              <li className='list-none flex justify-start items-center gap-4'>
                 <img src={Phone} alt="Phone icon" className={FooterStyles.footericon} />
                 xxx xxxxxxx
               </li>
             </span>
+
             <span className='m-2'>
-              <li className='list-none flex justify-start items-center gap-6 font-bold'>
+              <li className='list-none flex justify-start items-center gap-4'>
                 <img src={Envelope} alt="Envelope icon" className={FooterStyles.footericon} />
                 niko.xxxxxxxx@gmail.com
               </li>
             </span>
-          </div>
+
+        </div>
+        <div className={FooterStyles.footer}>
+          <span className='m-2'>
+            <li className='list-none flex justify-start items-center gap-4 md:w-100'>
+            © 2025 Niko Putkinen / PNP-POWER
+            </li>
+            </span>
+            {/* <span className='m-2'>
+            <li className='list-none flex justify-start items-center gap-4 md:w-100'>
+            Kaikki oikeudet pidätetään
+            </li>
+          </span> */}
+        </div>
     </div>
   )
 }
--- src/components/GalleryImages.jsx
+++ src/components/GalleryImages.jsx
@@ -1,18 +1,49 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function GalleryImages() {
 
-    // const [activePicture, setActivePicture] = useState(null);
-    // const [enlargedPicture, setEnlargedPicture] = useState(null);
+    const [activePicture, setActivePicture] = useState(null);
+    const [enlargedPicture, setEnlargedPicture] = useState(null);
 
-    
+    const images = [
+        { id: 1, src: '../../public/turbosystems.jpg', alt: 'pakosarjat' },
+        { id: 2, src: '../../public/pakosarjat.jpg', alt: 'pakosarjat' },
+        { id: 3, src: '../../public/img15.jpg', alt: 'kolmonen' },
+      ];
 
-  return (
-    <div>
-        {/* <div>GalleryImages</div> */}
-        <div className="gallerypictures grid justify-center">
-                <img src="../../public/turbosystems.jpg" width="225px" height="300px" alt="pakosarjat" />
+      const handlePictureClick = (picture) => {
+        if (enlargedPicture === picture) {
+          setEnlargedPicture(null);
+        } else {
+          setActivePicture(picture);
+          setEnlargedPicture(picture);
+        }
+      }
+
+      const handlePictureMouseLeave = (picture) => {
+        setEnlargedPicture(null);
+    };
+
+    return (
+        <div className="gallerypictures">
+          {images.map((image) => (
+            <img
+              key={image.id}
+              src={image.src}
+              width="225px"
+              height="300px"
+              alt={image.alt}
+              className={activePicture === image.id ? 'active' : ''}
+              style={{
+                transform: enlargedPicture === image.id ? 'scale(2.5)' : 'scale(1)',
+                transition: 'transform 0.25s ease',
+                zIndex: enlargedPicture === image.id ? 1000 : '',
+              }}
+              onClick={() => handlePictureClick(image.id)}
+              onMouseLeave={() => handlePictureMouseLeave(image.id)}
+            />
+          ))}
         </div>
-        </div>
-  )
-}
+      );
+    }
